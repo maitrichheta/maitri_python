91dabd72f8acf20efdd7e646ad7b75cb35f697d3
maitrichheta <maitrichheta6@gmail.com>
2025-06-11 12:20:07 +0530

    L-8

---
+++ L-9.py
@@ -0,0 +1,37 @@
+# String Formating
+
+#  String Value using f charactor
+
+# Name = "Maitri"
+# Age = 19
+
+# Profile = "My Name is {} and My Age is {}".format(Name , Age)
+
+# print(Profile)
+
+# String with indexing ang slicing
+
+statement = "Hello Python!"
+
+# print(statement[-2])
+# print(statement[0])
+# print(statement[1])
+# print(statement[2])
+# print(statement[3])
+# print(statement[4])
+# print(statement[5])
+
+# print(statement[2:6])
+# print(statement[2:8])
+# print(statement[-5:-1])
+# print(statement[1:- 6])
+
+# list
+
+lists = [10 , 20 , 30]
+lists = [10 , 20 , "Hello"]
+
+print(lists)
+print(lists[0])
+print(lists[1])
+print(lists[2])
