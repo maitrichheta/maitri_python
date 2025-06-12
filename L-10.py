096d010e41a449decea1ed63a7bfb4d0979646d6
maitrichheta <maitrichheta6@gmail.com>
2025-06-12 13:05:10 +0530

    L-10

---
+++ L-10.py
@@ -0,0 +1,88 @@
+# List
+# Tuple
+# Set
+# Dictionary
+
+
+# List
+# Mutable
+
+# Example
+
+# my_list = [1 , 2 , 3]
+
+# my_list[2] = 10
+
+# my_list.append(20)
+
+# my_list.append(20)
+
+# del my_list[0]
+
+# print(my_list)
+
+# Tuple
+# Immutable
+
+# my_tuple = (1 , 2 , 3)
+
+# print(my_tuple)
+
+# Set 
+# Mutable
+
+# my_set = {1 , 2 , 3}
+
+# my_set.add(10)
+
+# my_set.add(10)
+
+# print(my_set)
+
+# Dictionary
+# Mutable
+ 
+# my_dics = {"1":"alice" , "2":"vishakha"}
+
+# my_dics["1"] = "india"
+
+# my_dics["3"] = "English"
+
+# del my_dics["2"]
+
+# print(my_dics)
+
+# name1 = "Taxil"
+# name2 = "Manan"
+# name3 = "Harshil"
+# name4 = "Prit"
+# name5 = "Falgun"
+
+# print(name1)
+# print(name2)
+# print(name3)
+# print(name4)
+# print(name5)
+
+# name_list = ["Taxil" , "Manan" , "Harshil" , "Prit" , "Falgun"]
+
+# print(name_list[0])
+# print(name_list[1])
+# print(name_list[2])
+# print(name_list[3])
+# print(name_list[4])
+
+# for user in name_list:
+#    print(user)
+
+# Type casting constructor
+
+a = [1 , 2 , 3]
+b = tuple(a)
+c = set(b)
+d = dict(c)
+
+print(b)
+print(c)
+print(d)
+
